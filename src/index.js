dab5567bdeb23110fdd74c76648e9104f6cea098
MackRoe <elaine.music@students.makeschool.com>
2021-05-06 18:47:37 -0400

    schema and resolver for feed added

--- src/index.js
+++ src/index.js
@@ -1,20 +1,40 @@
 const { ApolloServer } = require('apollo-server');
 
-// 1
+// Schemas
 const typeDefs = `
-  type Query {
-    info: String!
-  }
+type Query {
+  info: String!
+  feed: [Link!]!
+}
+
+type Link {
+  id: ID!
+  description: String!
+  url: String!
+}
 `
 
-// 2
+let links = [{
+  id: 'link-0',
+  url: 'www.howtographql.com',
+  description: 'Fullstack tutorial for GraphQL'
+}]
+
+
+// Resolvers
 const resolvers = {
   Query: {
-    info: () => `This is the API of a Hackernews Clone`
+    info: () => null,
+    feed: () => links,
+},
+  Link: {
+      id: (parent) => parent.id,
+      description: (parent) => parent.description,
+      url: (parent) => parent.url,
   }
 }
 
-// 3
+// Start Server
 const server = new ApolloServer({
   typeDefs,
   resolvers,
