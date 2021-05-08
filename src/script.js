6bab784a91f8d47fccde46353f724c0e0e471ca2
MackRoe <elaine.music@students.makeschool.com>
2021-05-08 08:26:56 -0400

    add newLink const to main in src/script.js

--- src/script.js
+++ src/script.js
@@ -6,6 +6,12 @@ const prisma = new PrismaClient()
 
 // Send queries to db
 async function main() {
+  const newLink = await prisma.link.create({
+      data: {
+          description: 'Fullstack tutorial for GraphQL',
+          url: 'www.howtographql.com',
+      },
+  })
   const allLinks = await prisma.link.findMany()
   console.log(allLinks)
 }
